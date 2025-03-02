9be185351281ae8cc3e7b79b2b07c0697627c85f
Joanna Szych <joannaszych@gmail.com>
2025-03-02 16:06:35 +0100

    final testing and fixes

--- rasa_chatbot/actions/database/db_config.py
+++ rasa_chatbot/actions/database/db_config.py
@@ -1,5 +1,5 @@
 DB_CONFIG = {
-    'host': 'localhost',  # Database host
+    'host': 'postgres',  # Database host
     'database': 'rasa_db',  # Database name
     'user': 'rasa_user',  # Database user
     'password': 'rasa_password'  # Database password
